1a90a6e9fc981920ce2fc18d7a0e70447de6d1fe
AndreaB <128647787+andreaber23@users.noreply.github.com>
2023-09-10 19:23:18 -0400

    trying to deploy

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const path = require('path');
 const db = require('./config/connection');
 // const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
